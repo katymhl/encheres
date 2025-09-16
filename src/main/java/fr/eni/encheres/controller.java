464eac112883db9f9b45e63c4a3d757499ab399e
carolineaire <noblet.caroline.pro@gmail.com>
2025-09-16 09:48:27 +0200

    1ere mis en place du layout principal : layout.html, frag-entete, frag-piedpage, messages i18n, maj index (accueil)

--- src/main/java/fr/eni/encheres/controller.java
+++ src/main/java/fr/eni/encheres/controller.java
@@ -1,7 +1,9 @@
 package fr.eni.encheres;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
+@Controller
 public class controller {
 
 
