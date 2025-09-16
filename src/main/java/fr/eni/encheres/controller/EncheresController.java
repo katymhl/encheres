e4c262df89ecd9295cd53fd19f4cd521f2f80ff2
carolineaire <noblet.caroline.pro@gmail.com>
2025-09-16 16:55:52 +0200

    ajout de la connexion en cours (erreurs à corriger)

--- src/main/java/fr/eni/encheres/controller/EncheresController.java
+++ src/main/java/fr/eni/encheres/controller/EncheresController.java
@@ -4,8 +4,10 @@ import fr.eni.encheres.bll.UtilisateurService;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
+@RequestMapping("/enchere")
 public class EncheresController {
 UtilisateurService utilisateurService;
 public EncheresController(UtilisateurService utilisateurService){
