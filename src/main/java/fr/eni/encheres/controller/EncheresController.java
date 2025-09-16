1cfbdb3298a463329fa10516f8c7200a43a94cfe
carolineaire <noblet.caroline.pro@gmail.com>
2025-09-16 13:42:28 +0200

    suppression controller

--- src/main/java/fr/eni/encheres/controller/EncheresController.java
+++ src/main/java/fr/eni/encheres/controller/EncheresController.java
@@ -3,6 +3,8 @@ package fr.eni.encheres.controller;
 import fr.eni.encheres.bll.UtilisateurService;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
 @Controller
 public class EncheresController {
 UtilisateurService utilisateurService;
@@ -15,5 +17,10 @@ public EncheresController(UtilisateurService utilisateurService){
 
         System.out.println(utilisateurService.findById("coach_admin"));
         return "index.html";
+    }
+
+    @GetMapping("/{page}")
+    public String viewPage(@PathVariable String page) {
+        return page;
     }
 }
