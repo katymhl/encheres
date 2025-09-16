d2976b452147fd05b17e646bb6a651552762b0df
carolineaire <noblet.caroline.pro@gmail.com>
2025-09-16 10:55:00 +0200

    modification des liens dans le fragment-entete

--- src/main/java/fr/eni/encheres/controller.java
+++ src/main/java/fr/eni/encheres/controller.java
@@ -2,6 +2,7 @@ package fr.eni.encheres;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 
 @Controller
 public class controller {
@@ -9,8 +10,11 @@ public class controller {
 
     @GetMapping
     public String getDetail() {
-
-
         return "index.html";
+    }
+
+    @GetMapping("/{page}")
+    public String viewPage(@PathVariable String page) {
+        return page;
     }
 }
