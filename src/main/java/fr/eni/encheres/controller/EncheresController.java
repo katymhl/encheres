8d4b23df1a3efd03eec58982d9ab209ddf764dd8
carolineaire <noblet.caroline.pro@gmail.com>
2025-09-17 12:19:33 +0200

    ajout des liens + initialisation branche

--- src/main/java/fr/eni/encheres/controller/EncheresController.java
+++ src/main/java/fr/eni/encheres/controller/EncheresController.java
@@ -16,6 +16,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 import java.util.List;
+import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class EncheresController {
