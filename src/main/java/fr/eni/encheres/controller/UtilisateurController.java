e381b1be2c948f1d25a028a55e118c21cbf40de1
carolineaire <noblet.caroline.pro@gmail.com>
2025-09-18 16:49:29 +0200

    gestion de l'apparition des boutons selon le profile

--- src/main/java/fr/eni/encheres/controller/UtilisateurController.java
+++ src/main/java/fr/eni/encheres/controller/UtilisateurController.java
@@ -2,6 +2,8 @@ package fr.eni.encheres.controller;
 
 import fr.eni.encheres.bll.UtilisateurService;
 import fr.eni.encheres.bo.Utilisateur;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -18,6 +20,14 @@ public class UtilisateurController {
     public String afficherMonProfil(@RequestParam(name= "pseudo", required = true)String pseudo, Model model) {
         Utilisateur utilisateur = utilisateurService.findById(pseudo);
         model.addAttribute("user", utilisateur);
+
+        // Vérifier si l'utilisateur connecté est le même que celui consulté
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        boolean isOwnProfile = pseudo.equals(authentication.getName());
+
+        // Ajouter à l'attribut modèle isOwnProfile pour le contrôle dans Thymeleaf
+        model.addAttribute("isOwnProfile", isOwnProfile);
+
         return "profil";
     }
 
@@ -28,6 +38,15 @@ public class UtilisateurController {
             return "index";
         }
         model.addAttribute("user", utilisateur);
+
+        // Vérifier si l'utilisateur connecté est le même que celui consulté
+        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
+        boolean isOwnProfile = id_utilisateur.equals(authentication.getName());
+
+        // Ajouter à l'attribut modèle isOwnProfile pour le contrôle dans Thymeleaf
+        model.addAttribute("isOwnProfile", isOwnProfile);
+
+
         return "profil";
     }
 }
